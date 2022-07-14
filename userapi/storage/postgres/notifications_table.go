db3677b304e66808a2e7915ad86deb5c6defbd9e
Piotr Kozimor <p1996k@gmail.com>
2022-07-14 17:31:17 +0200

    Count distinct roomID in select contifications count

--- userapi/storage/postgres/notifications_table.go
+++ userapi/storage/postgres/notifications_table.go
@@ -71,9 +71,9 @@ const selectNotificationSQL = "" +
 	") AND NOT read ORDER BY localpart, id LIMIT $4"
 
 const selectNotificationCountSQL = "" +
-	"SELECT COUNT(*) FROM userapi_notifications WHERE localpart = $1 AND (" +
+	"SELECT COUNT(DISTINCT(room_id)) FROM userapi_notifications WHERE localpart = $1 AND (" +
 	"(($2 & 1) <> 0 AND highlight) OR (($2 & 2) <> 0 AND NOT highlight)" +
-	") AND NOT read"
+	") AND NOT read GROUP BY room_id"
 
 const selectRoomNotificationCountsSQL = "" +
 	"SELECT COUNT(*), COUNT(*) FILTER (WHERE highlight) FROM userapi_notifications " +
