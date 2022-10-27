090ef30a5b71c65449b42193bf5abdfe215cc0f4
Piotr Kozimor <p1996k@gmail.com>
2022-10-27 15:52:28 +0200

    Fix syncapi multiroom unit tests

--- syncapi/types/multiroom_test.go
+++ syncapi/types/multiroom_test.go
@@ -9,7 +9,13 @@ import (
 
 func TestMarshallMultiRoom(t *testing.T) {
 	is := is.New(t)
-	m, err := json.Marshal(MultiRoom{"@3:example.com": map[string]MultiRoomContent{"location": MultiRoomContent(`{"foo":"bar"}`)}})
+	m, err := json.Marshal(
+		MultiRoom{
+			"@3:example.com": map[string]MultiRoomData{
+				"location": {
+					Content:   MultiRoomContent(`{"foo":"bar"}`),
+					Timestamp: 123,
+				}}})
 	is.NoErr(err)
-	is.Equal(m, []byte(`{"@3:example.com":{"location":{"foo":"bar"}}}`))
+	is.Equal(m, []byte(`{"@3:example.com":{"location":{"content":{"foo":"bar"},"timestamp":123}}}`))
 }
